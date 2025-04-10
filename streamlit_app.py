68b6ed88ec31487d95c99e5b6e6e8442a2d5fef0
Okere Constance Amarachi <120277665+Constyokere@users.noreply.github.com>
2025-04-10 23:50:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import pandas as pd
 st.title('🎈 Machine Learning App')
 
 st.info('This app builds machine learning models')
-df=pd.read_csv("C:/Users/MY COMPUTER/Documents/Lung Cancer Dataset.csv")
+df=pd.read_csv(http://localhost:8888/files/Desktop/ML/Lung%20Cancer%20Dataset.csv)
 df
