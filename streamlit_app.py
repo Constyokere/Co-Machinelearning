a2b17b614402a514232de71ccc33cc18420b9b41
Okere Constance Amarachi <120277665+Constyokere@users.noreply.github.com>
2025-04-11 05:32:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title('🎈 Machine Learning App')
 
 st.info('This app builds machine learning models')
 df=pd.read_csv('https://github.com/Constyokere/Lungcercancer-dataset/blob/main/Lung%20Cancer%20Dataset.csv')
+df
